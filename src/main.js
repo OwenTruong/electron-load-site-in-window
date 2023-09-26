d9c219769e12b0f5c2ce49104252b02ca37b234a
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-09-25 20:02:37 -0400

    managed to figure out how to embed 3rd party sites using BrowserView

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView } = require('electron');
 const path = require('node:path');
 
 const createWindow = () => {
@@ -19,11 +19,38 @@ const createWindowFromURL = (url) => {
     height: 600,
   });
   win.loadURL(url);
+  return win;
+};
+
+const createViewFromURL = (win, url) => {
+  const view = new BrowserView();
+  win.setBrowserView(view);
+  view.setBounds({
+    x: 0,
+    y: 0,
+    width: 300,
+    height: 300,
+  });
+  view.webContents.loadURL(url);
 };
 
 app.whenReady().then(() => {
   // createWindowFromURL('https://login.salesforce.com/');
-  createWindow();
+
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  const view = new BrowserView();
+  win.setBrowserView(view);
+  view.setBounds({
+    x: 400,
+    y: 0,
+    width: 300,
+    height: 600,
+  });
+  view.webContents.loadURL('https://www.linkedin.com/');
+  win.loadFile('public/index.html');
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
